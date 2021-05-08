eebcfce4dfed5f983c3ad5d6aa4ccf3e532c2bf0
MIDAS <mmdaswani9009@gmail.com>
2021-05-09 03:57:02 +0530

    first filter for spam check

--- bot_core.py
+++ bot_core.py
@@ -10,3 +10,17 @@ reddit = praw.Reddit(client_id = "{}".format((lines[0]).strip()),
                      password = "{}".format((lines[3]).strip()),
                      user_agent = "{}".format((lines[4]).strip()))
 
+def spamcheck(kw):
+    authorl = []
+    for submission in reddit.subreddit("all").search(kw, sort = "new", limit = 20):
+        print("\n\nTITLE: ", submission.title,
+              "\n\tAUTHOR: ", submission.author,
+              "\n\t LINK: ", submission.url)
+        if submission.author not in authorl:
+            authorl.append(submission.author)
+    return authorl
+
+if __name__ == "__main__":
+    authors = spamcheck("Free Udemy")
+    for author in authors:
+        print(author)
